d7b3c8df1b030d3e296a34ccaca25355887a46ca
odenktools <odenktools86@gmail.com>
2017-11-11 16:35:31 +0700

    Modify README.md

--- install-percona.sh
+++ install-percona.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A;
-echo "deb http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list;
-echo "deb-src http://repo.percona.com/apt `lsb_release -cs` main" >> /etc/apt/sources.list.d/percona.list;
-sudo apt-get update;
-sudo apt-get -y install percona-xtradb-cluster-client-5.5 percona-xtradb-cluster-server-5.5 percona-xtrabackup percona-toolkit;
+wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
+sudo apt-get update
+sudo apt-get -y install libaio1 percona-xtradb-cluster-client-5.5 percona-xtradb-cluster-server-5.5 percona-xtrabackup percona-toolkit;
 sudo mysql -e "CREATE FUNCTION fnv1a_64 RETURNS INTEGER SONAME 'libfnv1a_udf.so'";
 sudo mysql -e "CREATE FUNCTION fnv_64 RETURNS INTEGER SONAME 'libfnv_udf.so'";
 sudo mysql -e "CREATE FUNCTION murmur_hash RETURNS INTEGER SONAME 'libmurmur_udf.so'";
 sudo mysql -u root -e "CREATE USER 'root'@'%'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;";
 sudo mysql -u root -e "CREATE USER 'sstuser'@'localhost' IDENTIFIED BY 's3cretPass'; GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sstuser'@'localhost'; FLUSH PRIVILEGES;";
 sudo mysql -u root -e "GRANT process ON *.* to 'clusterCheck'@'localhost' IDENTIFIED BY 'clusterpwd!'; FLUSH PRIVILEGES;";
+
+echo "Bind MySQL Address to 0.0.0.0"
+
+sed -Ei 's/^(bind-address|log)/#&/' /etc/mysql/my.cnf && \
+  sed -Ei 's/#bind-address\s+=\s+127.0.0.1/bind-address=0.0.0.0/g' /etc/mysql/my.cnf
+
+sudo /etc/init.d/mysql restart
